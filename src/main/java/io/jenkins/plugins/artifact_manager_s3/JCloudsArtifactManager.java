050dde00e0cf70adb0fa4b42533266ecd7ea1591
Carlos Sanchez <carlos@apache.org>
2018-04-09 19:57:25 +0200

    Fix failing test outside jenkins

--- src/main/java/io/jenkins/plugins/artifact_manager_s3/JCloudsArtifactManager.java
+++ src/main/java/io/jenkins/plugins/artifact_manager_s3/JCloudsArtifactManager.java
@@ -142,14 +142,13 @@ class JCloudsArtifactManager extends ArtifactManager implements StashManager.Sta
 
     @Override
     public boolean delete() throws IOException, InterruptedException {
-        return delete(getBlobPath(""));
+        return delete(getContext(getExtension(PROVIDER).getCredentialsSupplier()).getBlobStore(), getBlobPath(""));
     }
 
     /**
      * Delete all blobs starting with prefix
      */
-    static boolean delete(String prefix) throws IOException, InterruptedException {
-        BlobStore blobStore = getContext(getExtension(PROVIDER).getCredentialsSupplier()).getBlobStore();
+    static boolean delete(BlobStore blobStore, String prefix) throws IOException, InterruptedException {
         Iterator<StorageMetadata> it = new JCloudsBlobStore.PageSetIterable(blobStore, BLOB_CONTAINER, ListContainerOptions.Builder.prefix(prefix).recursive());
         boolean found = false;
         while (it.hasNext()) {
