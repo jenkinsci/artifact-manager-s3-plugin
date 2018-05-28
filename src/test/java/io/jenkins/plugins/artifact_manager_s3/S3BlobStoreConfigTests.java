c4207f831618010543cde78f842f5e4ee974d986
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-05-28 18:44:58 +0200

    test improvement suggestions

--- src/test/java/io/jenkins/plugins/artifact_manager_s3/S3BlobStoreConfigTests.java
+++ src/test/java/io/jenkins/plugins/artifact_manager_s3/S3BlobStoreConfigTests.java
@@ -2,28 +2,60 @@ package io.jenkins.plugins.artifact_manager_s3;
 
 import java.util.logging.Logger;
 import io.jenkins.plugins.artifact_manager_jclouds.JCloudsArtifactManagerFactory;
+import org.apache.commons.lang.StringUtils;
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import jenkins.model.ArtifactManagerConfiguration;
+import static io.jenkins.plugins.artifact_manager_s3.S3BlobStore.KEY_CONTAINER;
+import static io.jenkins.plugins.artifact_manager_s3.S3BlobStore.KEY_PREFIX;
+import static io.jenkins.plugins.artifact_manager_s3.S3BlobStore.KEY_REGION;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class S3BlobStoreConfigTests {
 
-    protected static final Logger LOGGER = Logger.getLogger(JCloudsVirtualFileTest.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(JCloudsVirtualFileTest.class.getName());
+
     public static final String CONTAINER_NAME = "container-name";
     public static final String CONTAINER_PREFIX = "container-prefix";
     public static final String CONTAINER_REGION = "us-west-1";
 
+    public static final String ORG_CONTAINER = System.getProperty(KEY_CONTAINER);
+    public static final String ORG_PREFIX = System.getProperty(KEY_PREFIX);
+    public static final String ORG_REGION = System.getProperty(KEY_REGION);
+    public static final String MANUAL = "-Manual";
+
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
+    @After
+    public void cleanup(){
+        if (StringUtils.isBlank(ORG_CONTAINER)){
+            System.clearProperty(KEY_CONTAINER);
+        } else {
+            System.setProperty(KEY_CONTAINER, ORG_CONTAINER);
+        }
+
+        if (StringUtils.isBlank(ORG_PREFIX)){
+            System.clearProperty(KEY_PREFIX);
+        } else {
+            System.setProperty(KEY_PREFIX, ORG_PREFIX);
+        }
+
+        if (StringUtils.isBlank(ORG_REGION)){
+            System.clearProperty(KEY_REGION);
+        } else {
+            System.setProperty(KEY_REGION, ORG_PREFIX);
+        }
+    }
+
     @Test
     public void checkConfigurationFromProperties() {
-        System.setProperty(S3BlobStore.class.getName() + ".container", CONTAINER_NAME);
-        System.setProperty(S3BlobStore.class.getName() + ".prefix", CONTAINER_PREFIX);
-        System.setProperty(S3BlobStore.class.getName() + ".region", CONTAINER_REGION);
+        System.setProperty(KEY_CONTAINER, CONTAINER_NAME);
+        System.setProperty(KEY_PREFIX, CONTAINER_PREFIX);
+        System.setProperty(KEY_REGION, CONTAINER_REGION);
 
         S3BlobStore provider = new S3BlobStore();
         JCloudsArtifactManagerFactory artifactManagerFactory = new JCloudsArtifactManagerFactory(provider);
@@ -40,19 +72,19 @@ public class S3BlobStoreConfigTests {
     @Test
     public void checkConfigurationManually() {
         S3BlobStore provider = new S3BlobStore();
-        provider.setContainer(CONTAINER_NAME);
-        provider.setPrefix(CONTAINER_PREFIX);
-        provider.setRegion(CONTAINER_REGION);
+        provider.setContainer(CONTAINER_NAME + MANUAL);
+        provider.setPrefix(CONTAINER_PREFIX + MANUAL);
+        provider.setRegion(CONTAINER_REGION + MANUAL);
 
         JCloudsArtifactManagerFactory artifactManagerFactory = new JCloudsArtifactManagerFactory(provider);
         ArtifactManagerConfiguration.get().getArtifactManagerFactories().add(artifactManagerFactory);
 
         LOGGER.info(artifactManagerFactory.getProvider().toString());
 
-        assertEquals(artifactManagerFactory.getProvider().getContainer(), CONTAINER_NAME);
-        assertEquals(artifactManagerFactory.getProvider().getPrefix(), CONTAINER_PREFIX);
+        assertEquals(artifactManagerFactory.getProvider().getContainer(), CONTAINER_NAME + MANUAL);
+        assertEquals(artifactManagerFactory.getProvider().getPrefix(), CONTAINER_PREFIX + MANUAL);
         assertTrue(artifactManagerFactory.getProvider() instanceof S3BlobStore);
-        assertEquals(((S3BlobStore)artifactManagerFactory.getProvider()).getRegion(), CONTAINER_REGION);
+        assertEquals(((S3BlobStore)artifactManagerFactory.getProvider()).getRegion(), CONTAINER_REGION + MANUAL);
         LOGGER.info(artifactManagerFactory.getProvider().toString());
     }
 }
