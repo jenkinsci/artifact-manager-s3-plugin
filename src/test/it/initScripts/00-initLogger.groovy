6244e7b0d35401795da1d37b8f90b59d74e929ca
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-06-29 19:41:08 +0200

    change to cdKata

--- src/test/it/initScripts/00-initLogger.groovy
+++ src/test/it/initScripts/00-initLogger.groovy
@@ -1,19 +1,13 @@
-import java.util.logging.ConsoleHandler
 import java.util.logging.FileHandler
-import java.text.MessageFormat
 import java.util.logging.SimpleFormatter
-import java.util.logging.LogManager
-import jenkins.model.Jenkins
-import java.util.logging.LogRecord
-
-// Log into the console
-def WebAppMainLogger = LogManager.getLogManager().getLogger("hudson.WebAppMain")
-WebAppMainLogger.addHandler (new ConsoleHandler())
+import java.util.logging.Logger
+import java.util.logging.Level
 
 // Log into a file
-def RunLogger = LogManager.getLogManager().getLogger("hudson.model.Run")
+Logger glogger= Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+glogger.setLevel(Level.INFO);
 FileHandler handler = new FileHandler("/var/jenkins_home/jenkins.log", 1024 * 1024, 10, true);
 def formatter= (new SimpleFormatter());
 
 handler.setFormatter(formatter);
-RunLogger.addHandler(handler);
+glogger.addHandler(handler);
--- src/test/it/initScripts/02-createjobs.groovy
+++ src/test/it/initScripts/02-createjobs.groovy
@@ -12,24 +12,25 @@ println("-- Creating Jobs")
 if(Jenkins.instance.getItem("big-file") == null) {
     WorkflowJob project1 = Jenkins.instance.createProject(WorkflowJob.class, "ig-file")
     project1.definition = new CpsFlowDefinition(
-        "def name = 'test-s3-big-file'\n" +
-        "def file = 'test.bin'\n" + 
-        "timestamps {\n" +
-        " node() {\n" +
-        "   try{\n" +
-        "     stage(\"Generating ${file}\") {\n" +
-        "       sh \"[ -f ${file} ] || dd if=/dev/urandom of=${file} bs=10240 count=102400\"\n" +
-        "     }\n" +
-        "     stage('Archive') {\n" +
-        "        archiveArtifacts file\n" +
-        "      }\n" +
-        "      stage('Unarchive') {\n" +
-        "        unarchive mapping: [\"${file}\": 'test.bin']\n" +
-        "      }\n" +
-        "    } finally {\n" +
-        "      deleteDir()\n" +
-        "    }\n" +
-        "  }\n",
+        '''def name = 'test-s3-big-file'
+        def file = 'test.bin' 
+        timestamps {
+         node() {
+           try{
+             stage("Generating ${file}") {
+               sh "[ -f ${file} ] || dd if=/dev/urandom of=${file} bs=10240 count=102400"
+             }
+             stage('Archive') {
+                archiveArtifacts file
+              }
+              stage('Unarchive') {
+                unarchive mapping: ["${file}": 'test.bin']
+              }
+            } finally {
+              deleteDir()
+            }
+          }
+          ''',
         true // Sandbox
     )
     project1.save()
@@ -38,27 +39,28 @@ if(Jenkins.instance.getItem("big-file") == null) {
 if(Jenkins.instance.getItem("small-files") == null) {
     WorkflowJob project1 = Jenkins.instance.createProject(WorkflowJob.class, "small-files")
     project1.definition = new CpsFlowDefinition(
-      "def name = 'test-s3-big-file'\n" +
-      "def file = 'test.bin'\n" + 
-      "timestamps {\n" +
-      " node() {\n" +
-      "   try{\n" +
-      "     stage(\"Generating files\") {\n" +
-      "       for(def i = 1; i &lt; 100; i++) {\n" +
-      "         writeFile file: \"test/test-${i}.txt\", text: \"test ${i}\"\n" +
-      "         }\n" +
-      "     }\n" +
-      "     stage('Archive') {\n" +
-      "        archiveArtifacts 'test/*'\n" +
-      "      }\n" +
-      "      stage('Unarchive') {\n" +
-      "        deleteDir()\n"
-      "        unarchive mapping: ['test/': '.']\n"
-      "      }\n" +
-      "    } finally {\n" +
-      "      deleteDir()\n" +
-      "    }\n" +
-      "  }\n",
+      '''def name = 'test-s3-big-file'
+      def file = 'test.bin' 
+      timestamps {
+       node() {
+         try{
+           stage("Generating files") {
+             for(def i = 1; i < 100; i++) {
+               writeFile file: "test/test-${i}.txt", text: "test ${i}"
+               }
+           }
+           stage('Archive') {
+              archiveArtifacts 'test/*'
+            }
+            stage('Unarchive') {
+              deleteDir()
+              unarchive mapping: ['test/': '.']
+            }
+          } finally {
+            deleteDir()
+          }
+        }
+      ''',
       true // Sandbox
     )
     project1.save()
@@ -67,26 +69,27 @@ if(Jenkins.instance.getItem("small-files") == null) {
 if(Jenkins.instance.getItem("stash") == null) {
     WorkflowJob project1 = Jenkins.instance.createProject(WorkflowJob.class, "stash")
     project1.definition = new CpsFlowDefinition(
-      "def name = 'test-s3-big-file'\n" +
-      "def file = 'test.bin'\n" + 
-      "timestamps {\n" +
-      " node() {\n" +
-      "   try{\n" +
-      "     stage(\"Generating files\") {\n" +
-      "       for(def i = 1; i &lt; 100; i++) {\n" +
-      "         writeFile file: \"test/test-${i}.txt\", text: \"test ${i}\"\n" +
-      "         }\n" +
-      "     }\n" +
-      "     stage('Archive') {\n" +
-      "        stash name: 'stuff', includes: 'test/'\n" +
-      "      }\n" +
-      "      stage('Unarchive') {\n" +
-      "        unstash name: 'stuff'\n" +
-      "      }\n" +
-      "    } finally {\n" +
-      "      deleteDir()\n" +
-      "    }\n" +
-      "  }\n",
+      '''def name = 'test-s3-big-file'
+      def file = 'test.bin' 
+      timestamps {
+       node() {
+         try{
+           stage("Generating files") {
+             for(def i = 1; i < 100; i++) {
+               writeFile file: "test/test-${i}.txt", text: "test ${i}"
+               }
+           }
+           stage('Archive') {
+              stash name: 'stuff', includes: 'test/'
+            }
+            stage('Unarchive') {
+              unstash name: 'stuff'
+            }
+          } finally {
+            deleteDir()
+          }
+        }
+      ''',
       true // Sandbox
     )
     project1.save()
