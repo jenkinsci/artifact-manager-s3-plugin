f71adde7383098be9942f90a759fb817b33d9b2b
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-07-03 23:08:01 +0200

    remove if

--- src/test/it/initScripts/01-enableArtifactManager.groovy
+++ src/test/it/initScripts/01-enableArtifactManager.groovy
@@ -4,11 +4,11 @@ import io.jenkins.plugins.artifact_manager_jclouds.JCloudsArtifactManagerFactory
 import io.jenkins.plugins.artifact_manager_jclouds.s3.S3BlobStore
 
 // Predefine storage and prefix when run in test on AWS
-if (Boolean.getBoolean("artifact-manager-s3.enabled")) {
+//if (Boolean.getBoolean("artifact-manager-s3.enabled")) {
     def factory = new JCloudsArtifactManagerFactory(new S3BlobStore());
     println("--- Enabling default artifact storage: ${factory.descriptor.displayName}")
 
     ArtifactManagerConfiguration.get().artifactManagerFactories.add(factory)
     S3BlobStoreConfig.get().setContainer("cloudbees-arch-us-west-2")
     S3BlobStoreConfig.get().setPrefix("integration-tests/")
-}
+//}
