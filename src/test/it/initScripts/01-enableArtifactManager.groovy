3f1e5bec917c0cd65b1d485eae9d3cba9eeed790
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-07-24 18:09:06 +0200

    set region to us-west-2

--- src/test/it/initScripts/01-enableArtifactManager.groovy
+++ src/test/it/initScripts/01-enableArtifactManager.groovy
@@ -10,3 +10,4 @@ println("--- Enabling default artifact storage: ${factory.descriptor.displayName
 ArtifactManagerConfiguration.get().artifactManagerFactories.add(factory)
 S3BlobStoreConfig.get().setContainer("cloudbees-arch-us-west-2")
 S3BlobStoreConfig.get().setPrefix("integration-tests/")
+S3BlobStoreConfig.get().setRegion('us-west-2')
