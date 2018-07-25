d160fe3b761b9b4e932058b98776467148329652
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-07-25 11:20:49 +0200

    update AMS3 1.1 version, change to environment variables configuration

--- src/test/it/initScripts/01-enableArtifactManager.groovy
+++ src/test/it/initScripts/01-enableArtifactManager.groovy
@@ -2,12 +2,13 @@ import io.jenkins.plugins.artifact_manager_jclouds.s3.S3BlobStoreConfig
 import jenkins.model.ArtifactManagerConfiguration
 import io.jenkins.plugins.artifact_manager_jclouds.JCloudsArtifactManagerFactory
 import io.jenkins.plugins.artifact_manager_jclouds.s3.S3BlobStore
+import io.jenkins.plugins.aws.global_configuration.CredentialsAwsGlobalConfiguration
 
-
-def factory = new JCloudsArtifactManagerFactory(new S3BlobStore());
+def factory = new JCloudsArtifactManagerFactory(new S3BlobStore())
 println("--- Enabling default artifact storage: ${factory.descriptor.displayName}")
 
 ArtifactManagerConfiguration.get().artifactManagerFactories.add(factory)
-S3BlobStoreConfig.get().setContainer("cloudbees-arch-us-west-2")
-S3BlobStoreConfig.get().setPrefix("integration-tests/")
-S3BlobStoreConfig.get().setRegion('us-west-2')
+S3BlobStoreConfig.get().setContainer(System.getenv("S3_CONTAINER"))
+S3BlobStoreConfig.get().setPrefix(System.getenv("S3_PREFIX"))
+
+CredentialsAwsGlobalConfiguration.get().setRegion(System.getenv("S3_REGION"))
